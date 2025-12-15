6c4869fb1237eed89244b97c0be45e38e93ad5b5
Nqnt41 <ryanpcoveny@gmail.com>
2025-12-15 02:22:45 -0500

    Completed project tasks.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,39 +7,50 @@ def emotion_detector(text_to_analyse):
     myobj =  { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
 
-    formatted_response = json.loads(response.text)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
-    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    else:
+        formatted_response = json.loads(response.text)
 
-    anger = emotion['anger']
-    disgust = emotion['disgust']
-    fear = emotion['fear']
-    joy = emotion['joy']
-    sadness = emotion['sadness']
+        emotion = formatted_response['emotionPredictions'][0]['emotion']
 
-    curr_max = anger
-    curr_emotion = 'anger'
+        anger = emotion['anger']
+        disgust = emotion['disgust']
+        fear = emotion['fear']
+        joy = emotion['joy']
+        sadness = emotion['sadness']
 
-    if (disgust > curr_max):
-        curr_max = disgust
-        curr_emotion = 'disgust'
-    if (fear > curr_max):
-        curr_max = fear
-        curr_emotion = 'fear'
-    if (joy > curr_max):
-        curr_max = joy
-        curr_emotion = 'joy'
-    if (sadness > curr_max):
-        curr_max = sadness
-        curr_emotion = 'sadness'    
+        curr_max = anger
+        curr_emotion = 'anger'
 
-    dominant_emotion = curr_emotion
+        if (disgust > curr_max):
+            curr_max = disgust
+            curr_emotion = 'disgust'
+        if (fear > curr_max):
+            curr_max = fear
+            curr_emotion = 'fear'
+        if (joy > curr_max):
+            curr_max = joy
+            curr_emotion = 'joy'
+        if (sadness > curr_max):
+            curr_max = sadness
+            curr_emotion = 'sadness'    
 
-    return {
-        'anger': anger,
-        'disgust': disgust,
-        'fear': fear,
-        'joy': joy,
-        'sadness': sadness,
-        'dominant_emotion': dominant_emotion
-    }
+        dominant_emotion = curr_emotion
+
+        return {
+            'anger': anger,
+            'disgust': disgust,
+            'fear': fear,
+            'joy': joy,
+            'sadness': sadness,
+            'dominant_emotion': dominant_emotion
+        }
